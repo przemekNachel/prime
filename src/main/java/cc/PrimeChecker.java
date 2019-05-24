391786064a73d84e500d8c166cb714a562cb928b
przemekNachel <przemekNachel@gmail.com>
2019-05-24 10:13:20 +0200

    Add zero and negatives handling

--- src/main/java/cc/PrimeChecker.java
+++ src/main/java/cc/PrimeChecker.java
@@ -3,6 +3,7 @@ package cc;
 public class PrimeChecker {
 
     public boolean isPrime(int number) {
+        if (number < 1) return false;
         int dividers = 0;
         for(int divider = 1; divider <= number; divider++) {
             if(number%divider == 0) {
--- src/test/java/cc/PrimeCheckerTest.java
+++ src/test/java/cc/PrimeCheckerTest.java
@@ -14,6 +14,16 @@ public class PrimeCheckerTest {
     }
 
     @Test
+    public void test_negative() {
+        Assert.assertFalse(primeChecker.isPrime(-1));
+    }
+
+    @Test
+    public void test_zero() {
+        Assert.assertFalse(primeChecker.isPrime(0));
+    }
+
+    @Test
     public void test_one() {
         Assert.assertFalse(primeChecker.isPrime(1));
     }
@@ -44,7 +54,22 @@ public class PrimeCheckerTest {
     }
 
     @Test
+    public void test_eight() {
+        Assert.assertFalse(primeChecker.isPrime(8));
+    }
+
+    @Test
     public void test_seven() {
         Assert.assertTrue(primeChecker.isPrime(7));
+    }
+
+    @Test
+    public void test_eleven() {
+        Assert.assertTrue(primeChecker.isPrime(11));
+    }
+
+    @Test
+    public void test_thirteen() {
+        Assert.assertTrue(primeChecker.isPrime(13));
     }
 }
