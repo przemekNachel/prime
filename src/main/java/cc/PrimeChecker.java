1d3b579558f597cd7190f82b50dc0c4629d561d2
przemekNachel <przemekNachel@gmail.com>
2019-05-25 17:24:25 +0200

    Simplify more

--- src/main/java/cc/PrimeChecker.java
+++ src/main/java/cc/PrimeChecker.java
@@ -3,12 +3,11 @@ package cc;
 public class PrimeChecker {
 
     public boolean isPrime(int number) {
-        int dividers = 0;
-        for(int divider = 1; divider <= number; divider++) {
+        for(int divider = 2; divider < number; divider++) {
             if(number%divider == 0) {
-                dividers++;
+                return false;
             }
         }
-        return dividers == 2;
+        return number >= 2;
     }
 }
