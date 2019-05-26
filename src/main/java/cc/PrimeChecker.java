f41396eb95aac16ce373bc0caa077d463e3dabdb
przemekNachel <przemekNachel@gmail.com>
2019-05-26 09:16:48 +0200

    Remove checking dividers bigger than half of number

--- src/main/java/cc/PrimeChecker.java
+++ src/main/java/cc/PrimeChecker.java
@@ -3,7 +3,7 @@ package cc;
 public class PrimeChecker {
 
     public boolean isPrime(int number) {
-        for(int divider = 2; divider < number; divider++) {
+        for(int divider = 2; divider <= number / 2; divider++) {
             if(number%divider == 0) {
                 return false;
             }
